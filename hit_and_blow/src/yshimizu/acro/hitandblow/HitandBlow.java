6dead5e8213ff9549231a0ea9cce1e70cceb3379
karahiyo <a.ryuklnm@gmail.com>
2013-02-03 05:41:31 +0900

    add "hit and blow", "cue" commands.

--- hit_and_blow/src/yshimizu/acro/hitandblow/HitandBlow.java
+++ hit_and_blow/src/yshimizu/acro/hitandblow/HitandBlow.java
@@ -18,51 +18,105 @@ import java.util.Scanner;
 public class HitandBlow {
 
 	private static final int MAX_TRIALS = 10; // 最大試行可能回数
-	public static final int[] FINAL_ANSWER = new int[3]; // 正解
-	public static final int DIGIT = 3; // 回答の桁数
+	public static int DIGIT=10; // 回答の桁数
+	public static int[] FINAL_ANSWER = new int[DIGIT]; // 正解
+	
+	private static final int INPUT = 0;
+	private static final int GIVEUP = 1;
 
 	public static void main(String[] args) {
-		System.out.println("!!START!! ---->>>>");
-		System.out.print(FINAL_ANSWER[0]+FINAL_ANSWER[1]+FINAL_ANSWER[2]+"\n");
-
 		HitandBlow hb = new HitandBlow();
-		
+		hb.selectLevel();
 		hb.randomSelect();
+		
+		System.out.println("!!START!! ---->>>>");
+		for(int i=0;i<DIGIT;i++)
+			System.out.print(FINAL_ANSWER[i]);
+		System.out.print("\n");
+
+		
 
 		for(int i=0;i<MAX_TRIALS;i++) {
 			System.out.printf("[phase %d]\n",i+1);
-			if(hb.challenge() == 1)
+			
+			System.out.print("[MENU]: (i)nput, (h)igh_and_low, (c)ue, (g)iveup ");
+			Scanner sc = new Scanner(System.in);
+			String menu;
+			menu = sc.next();
+			
+			switch (menu) {
+			case "i":
+				if(hb.challenge() == 1)
+					i=MAX_TRIALS;
 				break;
-				
+			case "h":
+				hb.highandLow();
+				i--;
+				break;
+			case "c":
+				hb.cue();
+				i--;
+				break;
+			case "g":
+				i=MAX_TRIALS;
+				System.out.printf("\nYOUR LOSE..\n   >> ");
+				break;
+			default:
+				System.out.printf("** Your command[%s] is not acceptable. Try again =>>\n",menu);
+				i--;
+				break;
+			}
+			
 		}
 		
-		System.out.printf("ANSWER is %d%d%d\n", FINAL_ANSWER[0],FINAL_ANSWER[1],FINAL_ANSWER[2]);
+		System.out.printf("ANSWER is ");
+		for(int i=0;i<DIGIT;i++)
+			System.out.printf("%d",FINAL_ANSWER[i]);
 
-		System.out.println(">>>>---- !!FINISH!!");
+		System.out.println("\n>>>>---- !!FINISH!!");
+	}
+	
+	public void selectLevel() {
+		System.out.print("Select Level => ");
+		Scanner sc = new Scanner(System.in);
+		int level = sc.nextInt();
+		DIGIT = level;
 	}
 	
 	/**
-	 * randomSelect()
+	 * randomSelect(): 正解の値をランダムに生成する
 	 */
 	public void randomSelect() {
-		int fa[] = {0,0,0};
+		System.out.print("randomSelect()\n");
+		int fa[] = new int[DIGIT];
+		boolean ok;
 		Random rnd = new Random();
-		fa[0] = rnd.nextInt(10);
-		do {
-			fa[1] = rnd.nextInt(10);
-		} while(fa[0] == fa[1]);
-		do {
-			fa[2] = rnd.nextInt(10);
-		} while(fa[2]==fa[1] || fa[2]==fa[0]);
-		
-		for(int i=0;i<3;i++)
+		for(int i=0;i<DIGIT;i++) {
+			fa[i] = rnd.nextInt(10);
+			do {
+				ok = true;
+				fa[i] = rnd.nextInt(10);
+				for(int j=0;j<i;j++) {
+					if(fa[i] == fa[j])
+						ok = false;
+				}
+			} while(ok==false);
+		}
+
+		System.out.printf("DIGIT=%d\n",DIGIT);
+		for(int i=0;i<DIGIT;i++) {
+			System.out.printf("fa[%d]=%d\n",i,fa[i]);
 			FINAL_ANSWER[i] = fa[i];
+		}
 		return;
 	}
 	
 	/**
-	 * マッチングフェーズ
-	 * @return
+	 * challenge() : ユーザの入力を受け取り、正解と比較しHit数,Blow数を数える。
+	 * @param 
+	 * @return  int  1 --- Good job!! It's just a correct answer. 
+	 * 			     0 --- nice challenge.
+	 * 			    -1 --- bad challenge...
 	 */
 	public int challenge() {
 		int ans[] = new int[DIGIT];
@@ -80,28 +134,25 @@ public class HitandBlow {
 			return -1;
 		}
 		
-		if(ans_pre_check.length() != 3) {
+		if(ans_pre_check.length() != DIGIT) {
 			System.out.print("** The length of the integer you entered is incorrect.\n");
 			System.out.printf("**** ans.length = %d\n",ans_pre_check.length());
 			return -1;
 		}
-		
-		if(!ans_pre_check.matches("[0-9]{3}")){
+
+		if(!ans_pre_check.matches("[0-9]+")){
 			System.out.print("** Please enter an integer.\n");
 			return -1;
 		}
 		
-		int ans_pre_checked;
-		ans_pre_checked = Integer.parseInt(ans_pre_check);
-		
 		for(int i=0;i<DIGIT;i++) {
-			ans[i] = intAt(ans_pre_checked,i);
+			ans[i] = Integer.parseInt(ans_pre_check.substring(i,i+1));
 		}
 		
 		for(int i=0;i<DIGIT-1;i++)
 			for(int j=i+1;j<DIGIT;j++) {
 				if(ans[i] == ans[j]) {
-					System.out.print("** Please enter a value that is different from three integers.\n");
+					System.out.printf("** Please enter a value that is different from %d integers.\n",DIGIT);
 					return -1;
 				}
 			}
@@ -111,13 +162,36 @@ public class HitandBlow {
 	}
 	
 	/**
+	 * high and low
+	 */
+	public void highandLow() {
+		for(int i=0;i<DIGIT;i++) {
+			if(FINAL_ANSWER[i] > 5)
+				System.out.printf("[%d]HIGH ",i);
+			else
+				System.out.printf("[%d]Low ", i);
+		}
+		System.out.print("\n");
+		return;
+	}
+	
+	public void cue() {
+		Scanner sc = new Scanner(System.in);
+		int p;
+		System.out.print("Which is the number you want to know? >>");
+		p = sc.nextInt();
+		System.out.printf("That is [%d].\n",FINAL_ANSWER[p]);
+		return;
+	}
+	
+	/**
 	 *   ユーザの回答を正解の値と比較、Hit数Blow数の判定
 	 * @param ans
 	 * @return 
 	 */
 	public int hb_check(int[] ans) {
 		boolean correct = true;
-		for(int i=0;i<3;i++) 
+		for(int i=0;i<DIGIT;i++) 
 			if (ans[i] != FINAL_ANSWER[i])
 				correct = false;
 		
@@ -139,7 +213,7 @@ public class HitandBlow {
 	 */
 	public int hit_count(int[] ans) {
 		int hits=0;
-		for(int i=0;i < DIGIT;i++) {
+		for(int i=0;i < DIGIT; i++) {
 			if( FINAL_ANSWER[i] == ans[i])
 				hits++;
 		}
@@ -171,16 +245,9 @@ public class HitandBlow {
 	public int intAt(int num, int index)
 	{
 		String s;
-		if(num < 100) {
-			if(index == 0) return 0; //百の桁がゼロの場合は別に処理
-			s = Integer.toString(num);
-			int r = Integer.parseInt(s.substring(index-1,index));
-			return r;
-		} else {
-			s = Integer.toString(num);
-			int r = Integer.parseInt(s.substring(index, index+1));
-			return r;
-		}
+		s = Integer.toString(num);
+		int r = Integer.parseInt(s.substring(index,index+1));
+		return r;
 	}
 }
 
